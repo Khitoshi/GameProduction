0e8a676290ce38abe01b5dd15258ac178a9a3136
yuwy0101 <112088742+yuwy0101@users.noreply.github.com>
2023-01-13 15:39:02 +0900

    no message

--- 卒業制作/Assets/Scripts/GameManager.cs
+++ 卒業制作/Assets/Scripts/GameManager.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(FadeService))] 
+[RequireComponent(typeof(FadeService))]
 [RequireComponent(typeof(GameStagingController))]
+[RequireComponent(typeof(StageProgressService))]
 public class GameManager : MonoBehaviour
 {
     public static FadeService fade_service_ { get; private set; }
 
     public static GameStagingController game_staging_controller_ { get; private set; }
+
+    public static StageProgressService StageProgressService { get; private set; }
 
     //ゲーム開始時直後のシーン読込前に呼ばれるようにする属性を指定
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]  //←関数の前にこれを付ける事でゲーム開始時のシーン読込前に呼ばれる
@@ -25,6 +28,7 @@ public class GameManager : MonoBehaviour
         //グローバルで利用するためのサービスを設定
         fade_service_ = gameManager.GetComponent<FadeService>();
         game_staging_controller_ = gameManager.GetComponent<GameStagingController>();
+        StageProgressService = gameManager.GetComponent<StageProgressService>();
 
 
     }
